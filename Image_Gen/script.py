be1c718ea2d4c6d9d284a1a699222803704548fe
Jonas Erthal <erthaljonas@gmail.com>
2018-07-20 10:38:28 -0400

    MVP first stages

---
+++ Image_Gen/script.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+import urllib.request
+import requests
+import random
+import json
+
+# PIL Loader Issue Workaround
+def register_extension(id, extension): Image.EXTENSION[extension.lower()] = id.upper()
+Image.register_extension = register_extension
+def register_extensions(id, extensions):
+  for extension in extensions: register_extension(id, extension)
+Image.register_extensions = register_extensions
+
+
+# API Key and URL
+KEY = "9594603-1ab4b7442c4491ab852ebd1ee"
+
+URL = "https://pixabay.com/api/?key="
+
+# Image Generator Helper Functions
+def prep_query_string(q="", category="", image_type="all"):
+    queries = {"&q=": q.replace(" ","+"), "&category=": category.replace(" ", "+"), "&image_type=": image_type.replace(" ", "+")}
+    return queries
+
+def pull_images(q="",category="",image_type="all"):
+    api_call = URL + KEY
+    queries = prep_query_string(q, category, image_type)
+    for query in queries:
+        api_call += (query + queries[query])
+        print(api_call)
+    api_call = requests.get(api_call)
+    return json.loads(api_call.text)
+
+def resize_logo(logo, basewidth):
+    logo_w, logo_h = logo.size
+    logo_wpercent = (basewidth / float(logo_w))
+    logo_hsize = int((float(logo_h) * float(logo_wpercent)))
+    logo = logo.resize((basewidth, logo_hsize), Image.ANTIALIAS)
+    return logo
+
+def create_image(image, logo_path):
+    # Reading image and logo first
+    image = Image.open(image)
+    logo = Image.open(logo_path)
+    # Getting image size
+    image_w, image_h = image.size
+    # Resizing Logo as needed:
+    basewidth = image_w // 4
+    logo = resize_logo(logo, basewidth)
+    # Targeting image area
+    box = (image_w - (image_w // 3), image_h // 10)
+    # Creating mask to paste without weird background
+    logo_mask = logo.convert("L")
+    # Pasting
+    image.paste(logo, box, logo)
+    return image
+
+
+# Image Generator Function
+def generate_image(search_term="", category="", image_type="all", logo_path='./test_logo.png'):
+    # Loading json results
+    images_json = pull_images(search_term, category, image_type)
+    # Selecting image randomly from json response:
+    rand_image = random.choice(images_json['hits'])
+    # Reading Image
+    image = urllib.request.urlopen(rand_image['largeImageURL'])
+    # Creating Final Result
+    final = create_image(image, logo_path)
+    plt.imshow(final)
+    return final
---
+++ MVP/text_script.py
@@ -0,0 +1,131 @@
+# Importing Dependencies
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import unidecode
+import pandas as pd
+import string
+import json
+import requests
+import random
+import re
+import time, math
+
+# __________________________________________________________
+
+# Getting characters data
+all_characters = string.printable
+n_characters = len(all_characters)
+
+# __________________________________________________________
+
+
+# Setting Inputs and Targets
+# Turn string into list of longs
+def char_tensor(string):
+    tensor = torch.zeros(len(string)).long()
+    for c in range(len(string)):
+        tensor[c] = all_characters.index(string[c])
+    return (tensor)
+
+# __________________________________________________________
+
+# Defining the RNN class
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+
+        self.encoder = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        input = self.encoder(input.view(1, -1))
+        output, hidden = self.gru(input.view(1, 1, -1), hidden)
+        output, hidden = self.gru(input.view(1, 1, -1), hidden)
+        output, hidden = self.gru(input.view(1, 1, -1), hidden)
+        output, hidden = self.gru(input.view(1, 1, -1), hidden)
+        output, hidden = self.gru(input.view(1, 1, -1), hidden)
+        output = self.decoder(output.view(1, -1))
+        return output, hidden
+
+    def init_hidden(self):
+        return (torch.zeros(self.n_layers, 1, self.hidden_size))
+
+# __________________________________________________________
+
+# Importing Trained Model:
+
+hidden_size = 100
+n_layers = 1
+
+decoder = RNN(n_characters, hidden_size, n_characters, n_layers)
+decoder.load_state_dict(torch.load('./rnn.py'))
+
+
+# __________________________________________________________
+
+# Setting up the evaluator
+def evaluate(prime_str='A', predict_len=100, temperature=0.8):
+    hidden = decoder.init_hidden()
+    prime_input = char_tensor(prime_str)
+    predicted = prime_str
+
+    # Use priming string to "build up" hidden state
+    for p in range(len(prime_str) - 1):
+        _, hidden = decoder(prime_input[p], hidden)
+        inp = prime_input[-1]
+
+    inp = prime_input[-1]
+
+    for p in range(predict_len):
+        output, hidden = decoder(inp, hidden)
+
+        # Sample from the network as a multinomial distribution
+        output_dist = output.data.view(-1).div(temperature).exp()
+        top_i = torch.multinomial(output_dist, 1)[0]
+
+        # Add predicted character to string and use as next input
+        predicted_char = all_characters[top_i]
+        predicted += predicted_char
+        inp = char_tensor(predicted_char)
+
+    return predicted
+
+# __________________________________________________________
+
+# Setting up Sentiment Analysis
+def get_sentiment(string):
+    r = requests.post("http://text-processing.com/api/sentiment/", data={'text': string})
+    return json.loads(r.text)
+
+def get_candidates(num_candidates=10, predict_len=10, temperature=0.8):
+    candidates = []
+    # Keep track of how many evaluations we calculate
+    sentiment_evaluations = 0
+    # Evaluate potential candidated until we have our desired amount
+    while len(candidates) != num_candidates:
+        prime_str = random.choice(string.ascii_uppercase)
+        sample = evaluate(prime_str, predict_len, temperature)
+
+        # With predicted sample, run through sentiment analysis
+        sentiment = get_sentiment(sample)
+        sentiment_evaluations += 1
+
+        # Finalizing candidate if it has a strong enough score
+        if sentiment['probability']['pos'] > 0.70:
+            print(sentiment['probability'])
+            candidates.append(sample)
+
+        # Stop early if calculate too many evaluations (request limit)
+        if sentiment_evaluations >= 2000:
+            print("too many attempts: " + str(sentiment_evaluations))
+            return candidates
+
+    print("number of sentiment evaluations done: " + str(sentiment_evaluations))
+    return candidates
